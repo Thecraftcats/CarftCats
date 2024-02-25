9a16326cc46188db88f7b2a441771f8fbbfac850
torama370@gmail.com <torama370@gmail.com>
2024-02-25 09:40:18 +0700

    add footer website

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const btn = document.getElementById('btns')
 new Typed('#typed',{
-  strings : ['Hi i am a front end developer 👋'],
+  strings : ['Hi i am a front end developer👋', 'I come from Indonesia🇮🇩 ', 'Nice to meet you'],
   typeSpeed : 100,
   delaySpeed : 8000,
   loop : true
